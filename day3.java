51b6366fcd106ed478da8f06304064a5c8ff7017
MAHESH <maheshshinde9100@gmail.com>
2024-11-23 20:11:40 +0530

    day2 leetcode

---
+++ day3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int count = 1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[count-1]){
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
