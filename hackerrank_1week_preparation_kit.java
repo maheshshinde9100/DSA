7ef6f4224db65e1a3efd60dc7dc6bf8221b6e01c
MAHESH <maheshshinde9100@gmail.com>
2024-11-23 20:39:11 +0530

    hackerrank_practice kit 1

---
+++ hackerrank_1week_preparation_kit.java
@@ -0,0 +1,31 @@
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    int p =0;
+    int n=0;
+    int z=0;
+    int s = arr.size();
+    for(int i=0;i<arr.size();i++){
+        if(arr.get(i)>0){
+            p=p+1;
+        }else if(arr.get(i)<0){
+            n=n+1;
+        }else if(arr.get(i)==0){
+            z=z+1;
+        }
+    }
+    float res1 = (float) p/s;
+    float res2 =  (float) n/s;
+    float res3 = (float) z/s;
+    
+    System.out.println(res1+"\n"+res2+"\n"+res3);
+    }
+
+}
