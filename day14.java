35ff49e1cb89bf7e333886c868b2dc9e3ef55c7e
MAHESH <maheshshinde9100@gmail.com>
2024-11-28 12:48:16 +0530

    day14 Arsh+ApnaClg #leetcode

---
+++ day14.java
@@ -0,0 +1,39 @@
+// Arsh
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> dups = new ArrayList<>();
+        int count=0;
+        // for(int n: nums){
+        //     count++;
+        //     for(int i=count;i<nums.length;i++){
+        //         if(n==nums[i]){
+        //             dups.add(n);
+        //         }
+        //     }
+        // }
+        // return dups;
+            for(int i=0;i<nums.length;i++){
+                 int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                dups.add(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return dups;
+    }
+}
+
+//Apna clg
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+                        if (minHeap.size() > k) {
+                minHeap.poll(); 
+            }
+        }
+        return minHeap.peek();
+    }
+}
