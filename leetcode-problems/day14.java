72aab5b4616a572b7a09806ea1e1307fc7f565b4
MAHESH <maheshshinde9100@gmail.com>
2025-02-17 22:11:19 +0530

    changes....

--- leetcode-problems/day14.java
+++ leetcode-problems/day14.java
@@ -1,19 +1,8 @@
 // Arsh
 import java.util.*;
-
 class Solution1 {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> dups = new ArrayList<>();
-        int count=0;
-        // for(int n: nums){
-        //     count++;
-        //     for(int i=count;i<nums.length;i++){
-        //         if(n==nums[i]){
-        //             dups.add(n);
-        //         }
-        //     }
-        // }
-        // return dups;
             for(int i=0;i<nums.length;i++){
                  int index = Math.abs(nums[i]) - 1;
             if (nums[index] < 0) {
@@ -26,13 +15,13 @@ class Solution1 {
     }
 }
 
-//Apna clg
+//Apna clg ----another problem
 class Solution2 {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int num : nums) {
             minHeap.offer(num);
-                        if (minHeap.size() > k) {
+            if (minHeap.size() > k) {
                 minHeap.poll(); 
             }
         }
