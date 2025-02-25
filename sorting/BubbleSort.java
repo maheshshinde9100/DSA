06c8159c96bf1106852067a70b2aedc7397a0681
MAHESH <maheshshinde9100@gmail.com>
2025-02-25 18:35:17 +0530

    .

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -7,16 +7,23 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args){
         int[] arr = {10,5,100,43,50,88};
+        boolean swapped;
+        // ascending
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length-1;j++){
-                if(arr[i]>arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    
+            swapped = false;
+            for(int j=1;j<arr.length-1;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;   
+                    swapped = true;
                 }
             }
+            if(!swapped){ //!false  = true
+                break;
+            }
         }
+        System.out.println(arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }
