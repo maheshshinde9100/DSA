0876bae7081498408008ff6d2d632ecf2cb567b2
MAHESH SHINDE <120265441+coder-mahi@users.noreply.github.com>
2025-03-20 19:31:22 +0530

    Create SymmetryTree.java

---
+++ tree/SymmetryTree.java
@@ -0,0 +1,24 @@
+class SymmetryTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isMirror(root.left,root.right);
+    }
+
+    private boolean isMirror(TreeNode t1,TreeNode t2){
+        if(t1==null&&t2==null){
+            return true;
+        }
+        if(t1==null||t2==null){
+            return false;
+        }
+        if(t1.val!=t2.val){
+            return false;
+        }
+      
+        boolean leftSubtreeCheck=isMirror(t1.left,t2.right);
+        boolean rightSubtreeCheck=isMirror(t1.right,t2.left);
+       return leftSubtreeCheck && rightSubtreeCheck;
+    }
+}
