dd3989d82dfee443c27568510b903fccb548dff5
MAHESH <maheshshinde9100@gmail.com>
2025-06-20 12:47:49 +0530

    Star patterns assignment solved 1 to 4 -- remaining 5 to 20

--- Patterns/StarPatterns.java
+++ Patterns/StarPatterns.java
@@ -1,4 +1,68 @@
 package Patterns;
 
 public class StarPatterns {
+    public static void main(String[] args) {
+//            1.  *****
+//                *****
+//                *****
+//                *****
+//                *****
+        System.out.println("First Pattern :> ");
+        firstPattern();
+//            2.  *
+//                **
+//                ***
+//                ****
+//                *****
+        System.out.println("Second Pattern :> ");
+        secondPattern();
+//            3.  *****
+//                ****
+//                ***
+//                **
+//                *
+        System.out.println("Third Pattern :> ");
+        thirdPattern();
+//    4.  1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
+        System.out.println("Fourth Pattern :> ");
+        fourthPattern();
+
+        //remaining from 5 to 20
+    }
+    static void firstPattern(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void secondPattern(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void thirdPattern(){
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void fourthPattern(){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
