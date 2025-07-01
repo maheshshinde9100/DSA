c1cac43a6543e8be3167ceb45a5597b0376f6ce5
MAHESH <maheshshinde9100@gmail.com>
2025-07-01 14:07:27 +0530

    practiced Recursion

---
+++ recursion/CountZero.java
@@ -0,0 +1,12 @@
+public class CountZero {
+    public static void main(String[] args) {
+        int n = 1003010; //4 zeroes
+        System.out.println(countZero(n,0));
+    }
+
+    static int countZero(int n,int count){
+        if(n==0) return count;
+        count += (n%10==0) ?1 :0;
+        return countZero(n/10, count);
+    }
+}
---
+++ recursion/ReverseNumberAndString.java
@@ -0,0 +1,27 @@
+public class ReverseNumberAndString {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(reverse(n,""));
+
+        String str = "mahesh";
+        System.out.println(reverseStr(str, new String(), str.length()-1));
+    }
+    //reverse number
+    static String reverse(int n,String rev){
+        if(n==0){
+            return rev;
+        }
+        return reverse(n/10, rev+=n%10);  
+        
+        // return reverseNum(n / 10, rev * 10 + n % 10); // put return type int for function
+    }
+
+    //reverse string-->
+    static String reverseStr(String s, String rev,int count){
+        if(count < 0){
+            return rev.toString();
+        }
+        rev += s.charAt(count);
+         return reverseStr(s, rev, count-1);
+    }
+}
