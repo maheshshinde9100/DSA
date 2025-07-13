21b018c2f24cda99ccba8d7dc172b4d80f6fda8f
MAHESH <maheshshinde9100@gmail.com>
2025-07-13 13:17:36 +0530

    optimized approach to find target in rotated Array using binary search using Recursion

--- recursion/BinarySearch.java
+++ recursion/BinarySearch.java
@@ -1,11 +1,14 @@
 package recursion;
 public class BinarySearch{
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int target = 9;
-        System.out.println("Binary Search : index > "+ binarySearch(arr,0,arr.length-1,target));
-        System.out.println("Linear Search : index > " +linearSearch(arr,0,target));
+//        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+//        int target = 9;
+//        System.out.println("Binary Search : index > "+ binarySearch(arr,0,arr.length-1,target));
+//        System.out.println("Linear Search : index > " +linearSearch(arr,0,target));
 
+        int[] rotatedArr = new int[]{6,7,8,9,1,2,3,4,5};
+        int target  = 4;
+        System.out.println("(RotatedArray) target found at index :> "+findInArray(rotatedArr,target,0,rotatedArr.length-1));
     }
 
     private static int binarySearch(int[] arr,int start,int end, int target){
@@ -30,5 +33,29 @@ public class BinarySearch{
             return index;
         }
         return linearSearch(arr, index+1, target);
+    }
+
+    //search in Rotated Array
+    static int findInArray(int[] arr,int target,int start,int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[start] <= arr[mid]) {
+            if (arr[start] < target && arr[mid] >= target) {
+                return findInArray(arr, target, start, mid - 1);
+            } else {
+                return findInArray(arr, target, mid + 1, end);
+            }
+        } else {
+            if (arr[mid] < target && arr[end] >= target) {
+                return findInArray(arr, target, mid + 1, end);
+            } else {
+                return findInArray(arr, target, start, mid - 1);
+            }
+        }
     }
 }
--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -1,11 +1,13 @@
-package recursion;
-
 public class Recursion {
     public static void main(String[] args) {
         // 1. reverse a number
         // 2. check the given number is palindrome or not
-        System.out.println("Reverse :> "+reverse(123)); // output: 321  //done
-        System.out.println("Is Palindrome :> "+isPalindrome(123));
+//        System.out.println("Reverse :> "+reverse(123)); // output: 321  //done
+//        System.out.println("Is Palindrome :> "+isPalindrome(123));
+
+        int[] arr = new int[]{4,5,6,7,1,2,3}; //search target(2) in arr
+        int target = 2;
+        System.out.println(findInArray(arr,target,0,arr.length-1));
     }
 
     //note: Always if required extra args then create helper functions
@@ -13,7 +15,6 @@ public class Recursion {
         int sum = 0;
         return revHelper(n,sum);
     }
-
     static int revHelper(int n,int sum){
         if(n==0){
             return sum;
@@ -22,8 +23,31 @@ public class Recursion {
         sum = sum * 10 + rem;
         return revHelper(n/10,sum);
     }
-
     static boolean isPalindrome(int n){
         return reverse(n)==n;
     }
+
+    static boolean findInArray(int[] arr,int target,int start,int end) {
+        if (start > end) {
+            return false;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) {
+            return true;
+        }
+        if (arr[start] <= arr[mid]) {
+            if (arr[start] < target && arr[mid] >= target) {
+                return findInArray(arr, target, start, mid - 1);
+            } else {
+                return findInArray(arr, target, mid + 1, end);
+            }
+        } else {
+            if (arr[mid] < target && arr[end] >= target) {
+                return findInArray(arr, target, mid + 1, end);
+            } else {
+                return findInArray(arr, target, start, mid - 1);
+            }
+        }
+    }
+
 }
